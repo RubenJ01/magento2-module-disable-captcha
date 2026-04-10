ba0cbdf495f662a63acc70021d6faa60e100c61e
RubenJ01 <rubeneekhof@gmail.com>
2026-04-10 08:48:31 +0200

    feat: initial Magento 2 disable captcha module

---
+++ Model/CaptchaConfigManager.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+namespace RJDS\DisableCaptcha\Model;
+
+use Magento\Framework\App\Config\Storage\WriterInterface;
+
+class CaptchaConfigManager
+{
+    /**
+     * @var array<string, string>
+     */
+    private const DISABLED_CONFIG = [
+        'customer/captcha/enable' => '0',
+        'customer/captcha/forms' => '',
+        'customer/captcha/mode' => 'after_fail',
+        'admin/captcha/enable' => '0',
+        'admin/captcha/forms' => '',
+        'admin/captcha/mode' => 'after_fail',
+        'msp_securitysuite_recaptcha/frontend/enabled' => '0',
+        'msp_securitysuite_recaptcha/backend/enabled' => '0',
+        'recaptcha_frontend/type_for/customer_login' => '0',
+        'recaptcha_frontend/type_for/customer_create' => '0',
+        'recaptcha_frontend/type_for/customer_forgot_password' => '0',
+        'recaptcha_frontend/type_for/customer_edit' => '0',
+        'recaptcha_frontend/type_for/contact' => '0',
+        'recaptcha_frontend/type_for/newsletter' => '0',
+        'recaptcha_frontend/type_for/product_review' => '0',
+        'recaptcha_frontend/type_for/sendfriend' => '0',
+        'recaptcha_frontend/type_for/wishlist' => '0',
+        'recaptcha_frontend/type_for/coupon_code' => '0',
+        'recaptcha_frontend/type_for/place_order' => '0',
+        'recaptcha_frontend/type_for/paypal_payflowpro' => '0',
+        'recaptcha_frontend/type_for/braintree' => '0',
+        'recaptcha_backend/type_for/user_login' => '0',
+        'recaptcha_backend/type_for/user_forgot_password' => '0',
+    ];
+
+    /**
+     * @var array<string, string>
+     */
+    private const ENABLED_CONFIG = [
+        'customer/captcha/enable' => '1',
+        'customer/captcha/mode' => 'after_fail',
+        'admin/captcha/enable' => '1',
+        'admin/captcha/mode' => 'after_fail',
+        'msp_securitysuite_recaptcha/frontend/enabled' => '1',
+        'msp_securitysuite_recaptcha/backend/enabled' => '1',
+        'recaptcha_frontend/type_for/customer_login' => 'recaptcha',
+        'recaptcha_frontend/type_for/customer_create' => 'recaptcha',
+        'recaptcha_frontend/type_for/customer_forgot_password' => 'recaptcha',
+        'recaptcha_frontend/type_for/customer_edit' => 'recaptcha',
+        'recaptcha_frontend/type_for/contact' => 'recaptcha',
+        'recaptcha_frontend/type_for/newsletter' => 'recaptcha',
+        'recaptcha_frontend/type_for/product_review' => 'recaptcha',
+        'recaptcha_frontend/type_for/sendfriend' => 'recaptcha',
+        'recaptcha_frontend/type_for/wishlist' => 'recaptcha',
+        'recaptcha_frontend/type_for/coupon_code' => 'recaptcha',
+        'recaptcha_frontend/type_for/place_order' => 'recaptcha',
+        'recaptcha_frontend/type_for/paypal_payflowpro' => 'recaptcha',
+        'recaptcha_backend/type_for/user_login' => 'recaptcha',
+        'recaptcha_backend/type_for/user_forgot_password' => 'recaptcha',
+    ];
+
+    /**
+     * @var WriterInterface
+     */
+    private $configWriter;
+
+    public function __construct(WriterInterface $configWriter)
+    {
+        $this->configWriter = $configWriter;
+    }
+
+    public function disable(): void
+    {
+        $this->saveConfig(self::DISABLED_CONFIG);
+    }
+
+    public function enable(): void
+    {
+        $this->saveConfig(self::ENABLED_CONFIG);
+    }
+
+    /**
+     * @param array<string, string> $config
+     */
+    private function saveConfig(array $config): void
+    {
+        foreach ($config as $path => $value) {
+            $this->configWriter->save($path, $value);
+        }
+    }
+}
---
+++ Plugin/ReCaptcha/SafeValidationConfigResolver.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace RJDS\DisableCaptcha\Plugin\ReCaptcha;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Exception\InputException;
+use Magento\ReCaptchaUi\Model\ValidationConfigResolver;
+use Magento\ReCaptchaValidationApi\Api\Data\ValidationConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use RJDS\DisableCaptcha\Model\ReCaptcha\NullValidationConfig;
+
+class SafeValidationConfigResolver
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var NullValidationConfig
+     */
+    private $nullValidationConfig;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        NullValidationConfig $nullValidationConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->nullValidationConfig = $nullValidationConfig;
+    }
+
+    /**
+     * Avoid hard crashes when a captcha form has an empty/invalid type.
+     *
+     * @param ValidationConfigResolver $subject
+     * @param callable $proceed
+     * @param string $key
+     * @return ValidationConfigInterface
+     * @throws InputException
+     */
+    public function aroundGet(ValidationConfigResolver $subject, callable $proceed, string $key): ValidationConfigInterface
+    {
+        try {
+            return $proceed($key);
+        } catch (InputException $exception) {
+            if ($this->isDisabledCaptchaType($key)) {
+                return $this->nullValidationConfig;
+            }
+
+            throw $exception;
+        }
+    }
+
+    private function isDisabledCaptchaType(string $key): bool
+    {
+        $frontendType = (string)$this->scopeConfig->getValue(
+            'recaptcha_frontend/type_for/' . $key,
+            ScopeInterface::SCOPE_WEBSITE
+        );
+        $backendType = (string)$this->scopeConfig->getValue('recaptcha_backend/type_for/' . $key);
+
+        return in_array($frontendType, ['', '0'], true) || in_array($backendType, ['', '0'], true);
+    }
+}
